0e381b7c392df9d043adff9e5c982adc74e08230
Chris Brandt <chris.brandt@renesas.com>
2020-05-01 11:18:48 -0400

    build_scripts: arm-trusted-firmware: add eMMC build option

--- build_scripts/arm-trusted-firmware/build.sh
+++ build_scripts/arm-trusted-firmware/build.sh
@@ -2,9 +2,11 @@
 
 # HiHope RZ/G2M
 #MACHINE=hihope-rzg2m
+#EMMC_BOOT=1
 
 # HiHope RZ/G2N
 #MACHINE=hihope-rzg2n
+#EMMC_BOOT=1
 
 # Silicon Linux RZ/G2E
 #MACHINE=ek874
@@ -99,6 +101,115 @@ PLATFORM=rcar
 # (USE_MULTIMEDIA, do not set if using ECC
 #RCAR_LOSSY_ENABLE=1
 
+# PLAT
+#	Set string is "rcar"
+
+# LOG_LEVEL
+#	The IPL provides logging functions ERROR(), NOTICE(), WARN(), INFO() and VERBOSE().
+#	50 is all functions for output logs
+#
+#	Log level   Valid logging function
+#	-------------------------------------------------------
+#	0           No functions output logs
+#	10          ERROR()
+#	20          ERROR(), NOTICE()
+#	30          ERROR(), NOTICE(), WARN()
+#	40          ERROR(), NOTICE(), WARN(), INFO()
+#	50          ERROR(), NOTICE(), WARN(), INFO(), VERBOSE()
+
+# DEBUG
+#	Set value is 0 or 1. 0 is a release build, and 1 is a debug build
+#	If LOG_LEVEL value hasn??t been set, LOG_LEVEL is set as the DEBUG value in the following table
+#
+#	DEBUG build    LOG_LEVEL  Valid logging functions
+#	-------------------------------------------------------
+#	0     release  20         ERROR(), NOTICE()
+#	1     debug    40         ERROR(), NOTICE(), WARN(), INFO()
+
+# SPD
+#	The IPL provides configuration to load image of BL32
+#	Set string is ??none?? or ??opteed??.
+#	"none" = Does not load the image of BL32
+#	"opteed" = Load the image of BL32 from HyperFlash / QSPI Flash / eMMC
+
+# RCAR_QOS_TYPE
+#	AXI-bus has the QoS arbitration to control latency and bandwidth.
+#	0 = Enable the QoS arbitration setting (Default setting)
+#	3 = Disable the QoS arbitration setting
+
+
+# RCAR_DRAM_SPLIT
+#	DRAM split setting in the SDRAM setting
+#	0 = Linear (No split)
+#	1 = 4 channel split
+#	2 = 2 channel split
+#	3 = R-Car H3: 4 channel split / R-Car M3: 2 channel split / R-Car M3N:Linear / R-Car E3: Linear
+
+# PMIC_ROHM_BD9571
+#	This option represents the range of code implementation depending on the PMIC (ROHM BD9571) mounted on the
+#	evaluation board described in 3.1 hardware environment.
+
+
+# RCAR_AVS_SETTING_ENABLE
+#	PMIC initial voltage setting
+#	This option must be set to disabled in the case of R-Car E3.
+#	0 = disable
+#	1 = enable
+
+
+# LSI
+#	G2E,G2M,G2N,G2H
+
+# RCAR_SA6_TYPE:
+#	0 = for Hyper Flash / QPSI Flash boot (Default setting)
+#	1 = for eMMC boot
+
+# RCAR_BL33_EXECUTION_EL
+#	Set the exception level of BL33
+#	0 = EL1 (Default setting)
+#	1 = EL2
+#
+
+# RCAR_SYSTEM_SUSPEND
+#	0 = No Suspend to RAM
+#	1 = Suspend to RAM
+
+# RCAR_REF_INT
+#	Select DRAM refresh interval.
+#	0 = Default setting (H3, M3, M3N 1.95us / E3 3.90us)
+#	1 = Optional setting (H3, M3, M3N 3.90us / E3 7.80us)
+
+# RCAR_REWT_TRAINING
+#	Select "periodic write DQ training" mode.
+#	0 = not available
+#	1 = available (Default setting)
+
+# RCAR_BL2_DCACHE
+#	enables MMU and D-cache (L1 data cache / L2 cache) on IPL.
+#	0 = Disable the D-cache setting (Default setting)
+#	1 = Enable the D-cache setting
+
+# RCAR_SA0_SIZE
+#	Switch the IPL size of dummy certificate
+#	0 = For R-Car E3, IPL size is 80KB.
+#	1 = For R-Car H3/M3/M3N, IPL size is 170KB. (Default setting)
+
+# RCAR_DRAM_DDR3L_MEMCONF
+#	Select DRAM memory size.
+#	0 = 1G Byte
+#	1 = 2G Byte (Default setting)
+
+# RCAR_DRAM_DDR3L_MEMDUAL
+#	Select the SoC output (the number of connected SDRAM on board)
+#	0 = CS0, ODT0 enable and CS1, ODT1 disable (SDRAM 2pieces)
+#	1 = CS0, ODT0, CS1, ODT1 enable (SDRAM 4pieces) (Default setting)
+
+# RCAR_DRAM_LPDDR4_MEMCONF
+#	(RZ/G2H only)
+#	The LPDDR4 settings code included in the IPL release supports 2 types of memory configurations for R-Car H3
+#	0 = 1G Byte x 4 channel : 4GB
+#	1 = 2G Byte x4 channel : 8GB (Default setting)
+
 # RZ/G2E
 if [ "$MACHINE" == "ek874" ] ; then
   ATFW_OPT="LSI=G2E RCAR_SA0_SIZE=0 RCAR_AVS_SETTING_ENABLE=0 RZG_EK874=1 PMIC_ROHM_BD9571=0 RCAR_SYSTEM_SUSPEND=0 RCAR_DRAM_DDR3L_MEMCONF=1 RCAR_DRAM_DDR3L_MEMDUAL=1 SPD="none""
@@ -106,6 +217,7 @@ fi
 
 #RZ/G2M
 if [ "$MACHINE" == "hihope-rzg2m" ] ; then
+  # The RZ/G2M v1.3 device mounted on the HiHope-RZG2M board has been found to be unable to boot unless "RCAR_SECURE_BOOT=0"
   ATFW_OPT="LSI=G2M RCAR_DRAM_SPLIT=2 RCAR_AVS_SETTING_ENABLE=0 RZG_HIHOPE_RZG2M=1 PMIC_ROHM_BD9571=0 RCAR_SYSTEM_SUSPEND=0 RCAR_SECURE_BOOT=0 SPD="none""
 fi
 
@@ -113,6 +225,12 @@ fi
 if [ "$MACHINE" == "hihope-rzg2n" ] ; then
 ATFW_OPT="LSI=G2N RCAR_AVS_SETTING_ENABLE=0 RZG_HIHOPE_RZG2N=1 PMIC_ROHM_BD9571=0 RCAR_SYSTEM_SUSPEND=0 SPD="none""
 fi
+
+# For eMMC boot, you need to set RCAR_SA6_TYPE=1
+if [ "$EMMC_BOOT" == "1" ] ; then
+  ATFW_OPT="$ATFW_OPT RCAR_SA6_TYPE=1"
+fi
+
 
 #ECC
 #ECC_OPTS="LIFEC_DBSC_PROTECT_ENABLE=0 RZG_DRAM_EK874_ECC=1 "
