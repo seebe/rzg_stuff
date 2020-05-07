4451e31c1705bd486bf873add0d51a9442c60fa1
Chris Brandt <chris.brandt@renesas.com>
2020-05-07 10:37:28 -0400

    Add image_creator

---
+++ image_creator/README.md
@@ -0,0 +1,29 @@
+# Image Creator
+
+## Overview
+
+This script will create a complete image file (sd_card.img) that can be programmed into an SD Card, eMMC or USB Flash drive using a tool such as Win32DiskImager or Rufus (for Windows) or 'dd' (in Linux). These programs provide a sector by sector copy, partition table and all.
+
+Since the default RZ/G boot process is to read the Device Tree and kernel from a FAT16 partition (partition 1) and then mount a root file system (ext3 or ext4) found on partition 2, your image must contain the Master Boot Record (MBR) and partition table at the very beginning of the drive (sector 0), and then the correctly formatted and populated partitions in the correct locations in the image file.
+
+One way to do this is to prepare an real SD Card the way you want it, then read it out sector by sector to a file (using dd for example). But, that takes time. Also, fragments of old "deleted files" might still exist through out the storage area, so when you try to compress your image, it will not compress down as much as it should.
+
+This script instead will:
+ - Create a blank (all zeros) file that will be our final image. Since any space that is not used will be 0x00, this will make your image file compress well
+ - Use fdisk to write a MBR and partition table to the file that will contain a FAT16 and ext3/4 partition
+ - Mount that formatted file as a loop device so that you can copy files into the partitions as if it were an actual directory on your machine.
+ - Finally the script will compress (zip) the image file for you so it is easier to store and send
+
+## Instructions
+
+Simple copy/paste a copy of 'example_config.txt' and edit it as needed. All the settings are explained inside that example file.
+To run the program, pass your configuration file that you just created on the command line of the script.
+Example:
+
+    $ cp example_config.txt my_demo_config.txt
+    $ gedit my_demo_config.txt
+    $ ./create_image.sh my_demo_config.txt
+
+Note that the script uses 'sudo', so you might be prompted to enter your account password.
+
+
---
+++ image_creator/create_image.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# pass a file that contains all the settings
+# For example: ./create_image.sh example_config.txt
+
+if [ "$1" == "" ] ; then
+	echo "ERROR: Please pass the location of a config file on the command line"
+	echo "       Example: $ ./create_image.sh example_config.txt"
+	exit
+fi
+
+source $1
+
+message() {
+
+echo -----------------------------------------
+echo $1
+echo -----------------------------------------
+
+}
+
+
+# test that we can created the output file
+DIR=$(dirname "${OUTFILE}")
+FILENAME=$(basename "${OUTFILE}")
+mkdir -p $DIR
+touch "$OUTFILE"
+
+if [ ! -e $OUTFILE ] ; then 
+  echo "ERROR: Cannot create output file ${OUTFILE}"
+  exit
+fi
+
+message "Creating an empty blank image file"
+
+# MB = 1000*1000
+# M = 1024*1024,
+# GB = 1000*1000*1000
+# G = 1024*1024*1024
+
+if [ "${TOTAL_IMAGE_SIZE: -1}" == "G" ] ; then
+  BS_SIZE=1M
+  COUNT=${TOTAL_IMAGE_SIZE::-1}
+  let COUNT*=1024
+fi
+if [ "${TOTAL_IMAGE_SIZE: -1}" == "M" ] ; then
+  BS_SIZE=1M
+  COUNT=${TOTAL_IMAGE_SIZE::-1}
+fi
+if [ "${TOTAL_IMAGE_SIZE: -2}" == "MB" ] ; then
+  BS_SIZE=1MB
+  COUNT=${TOTAL_IMAGE_SIZE::-2}
+fi
+if [ "${TOTAL_IMAGE_SIZE: -2}" == "GB" ] ; then
+  BS_SIZE=1MB
+  COUNT=${TOTAL_IMAGE_SIZE::-2}
+  let COUNT*=1000
+fi
+#echo TOTAL_IMAGE_SIZE=$TOTAL_IMAGE_SIZE ; echo BS_SIZE=$BS_SIZE ; echo COUNT=$COUNT
+
+echo dd if=/dev/zero of=$OUTFILE bs=$BS_SIZE count=$COUNT
+dd if=/dev/zero of=$OUTFILE bs=$BS_SIZE count=$COUNT
+
+# (Optional) Verify your file size
+#ls -l $OUTFILE
+
+# Create 2 primary partitions inside our image file
+# (FAT16) + (ext3/4)
+message "Creating partitions"
+echo -e "n\n p\n 1\n \n +${FAT_SIZE}\n"\
+        "n\n p\n 2\n \n \n"\
+        "t\n 1\n 6\n"\
+        "p\n w\n" | fdisk -u $OUTFILE
+
+# (Optional) Verify your file partitioning
+# fdisk -l $OUTFILE
+
+# Find the start address for each partition inside the file
+FAT_START=$(fdisk -l $OUTFILE | grep "6 FAT16" | awk '{print $2}')
+let FAT_START*=512
+EXT_START=$(fdisk -l $OUTFILE | grep "83 Linux" | awk '{print $2}')
+let EXT_START*=512
+
+# Loop mount our FAT16 partition.
+message "Loop mount and format at FAT16"
+sudo losetup -v -f -o $FAT_START  $OUTFILE
+
+# Find out our loop device name
+LOOP_DEVICE=$(losetup --list | grep $OUTFILE | awk '{print $1}');
+
+# format as FAT16
+sudo mkfs.vfat -F 16 -n $FAT_LABEL $LOOP_DEVICE
+
+# mount this loop device (partition) so we can copy files into it
+mkdir -p /tmp/loop_mount/fat16
+sudo mount $LOOP_DEVICE /tmp/loop_mount/fat16
+sudo cp -r $FAT_FILES/* /tmp/loop_mount/fat16
+sudo umount  /tmp/loop_mount/fat16
+
+# Release the loop device
+sudo losetup -d $LOOP_DEVICE
+
+# Loop mount our ext partition.
+message "Loop mount and format at ext3/ext4"
+sudo losetup -v -f -o $EXT_START  $OUTFILE
+
+# Find out our loop device name
+LOOP_DEVICE=$(losetup --list | grep $OUTFILE | awk '{print $1}');
+
+# format as ext3/ext4
+sudo mkfs.${EXT_TYPE} -L $EXT_LABEL $LOOP_DEVICE
+
+# mount this loop device (partition) so we can copy files into it
+mkdir -p /tmp/loop_mount/ext
+sudo mount $LOOP_DEVICE /tmp/loop_mount/ext
+sudo cp -r $EXT_FILES/* /tmp/loop_mount/ext
+sudo umount  /tmp/loop_mount/ext
+
+# Release the loop device
+sudo losetup -d $LOOP_DEVICE
+
+# Create bzip2
+if [ "$CREATE_BZ2" == "yes" ] ; then
+	message "Compressing image (bz2)"
+	if [ -e ${OUTFILE}.bz2 ] ; then
+	  rm ${OUTFILE}.bz2
+	fi
+	bzip2 -v -k $OUTFILE
+fi
+
+# Create zip
+if [ "$CREATE_ZIP" == "yes" ] ; then
+	message "Compressing image (zip)"
+	if [ -e ${OUTFILE}.zip ] ; then
+	  rm ${OUTFILE}.zip
+	fi
+	zip -j ${OUTFILE}.zip ${OUTFILE}
+fi
+
+message "done"
+
