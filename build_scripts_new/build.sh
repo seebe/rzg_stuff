64b6713d05d59222833818331d42046690df794b
Chris Brandt <chris.brandt@renesas.com>
2021-09-22 14:12:12 -0400

    Add new style of build scripts

---
+++ build_scripts_new/build.sh
@@ -0,0 +1,228 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------------
+# Please read the README.md file first for proper setup
+#---------------------------------------------------------------------------
+
+# This build script can be used to build
+#  * Trusted Firmware-A
+#  * u-boot
+#  * Renesas Flash Writer
+#  * Linux Kernel
+
+# Please read "Repository Installs.txt" to install the toolchains.
+
+# Please read "Toolchain Installs.txt" to install the toolchains.
+
+# The output files you need will be copied to the 'output_xxxxx' directory. xxx will be the name of your board.
+
+# Supported Boards
+#MACHINE=hihope-rzg2m	# HiHope RZ/G2M
+#MACHINE=hihope-rzg2n	# HiHope RZ/G2N
+#MACHINE=hihope-rzg2h	# HiHope RZ/G2H
+#MACHINE=ek874		# Silicon Linux RZ/G2E
+#MACHINE=smarc-rzg2l	# Renesas RZ/G2L EVK
+
+
+#----------------------------------------------
+# Default Settings
+#----------------------------------------------
+TFA_DIR_DEFAULT=rzg_trusted-firmware-a
+UBOOT_DIR_DEFAULT=renesas-u-boot-cip
+FW_DIR_DEFAULT=rzg2_flash_writer
+KERNEL_DIR_DEFAULT=rz_linux-cip
+OUT_DIR=output_${MACHINE}
+
+# Read in functions from build_common.sh
+if [ ! -e build_common.sh ] ; then 
+  echo -e "\n ERROR: File \"build_common.sh\" not found\n."
+  exit
+else
+  source build_common.sh
+fi
+
+
+# Toolchain Selection GUI
+# Since each sub-script will want to ask the user what toolchain to use, we will keep a common interface in this file.
+if [ "$1" == "toolchain_select" ] ; then
+  
+    SELECT=$(whiptail --title "Toolchain setup" --menu "You may use ESC+ESC to cancel.\nEnter the command line you want to run before build.\n" 0 0 0 \
+	"1  SDK Toolchain (Rocko/Poky 2.4.3)" "  /opt/poky/2.4.3/environment-setup-aarch64-poky-linux" \
+	"2  Linaro gcc-linaro-7.5.0-2019.12" "  /opt/linaro/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu" \
+	"3  ARM gcc-arm-10.2-2020.11" "  /opt/arm/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf" \
+	"0  (none)" "  No setup" \
+	3>&1 1>&2 2>&3)
+  RET=$?
+  if [ $RET -eq 0 ] ; then
+    case "$SELECT" in
+      1\ *)
+      		x_TOOLCHAIN_SETUP_NAME="SDK Toolchain (Poky 2.4.3)" ; x_TOOLCHAIN_SETUP="source /opt/poky/2.4.3/environment-setup-aarch64-poky-linux" ;;
+      2\ *)
+      		x_TOOLCHAIN_SETUP_NAME="Linaro gcc-linaro-7.5.0-2019.12" ; x_TOOLCHAIN_SETUP="PATH=/opt/linaro/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin:\$PATH ; export CROSS_COMPILE=aarch64-linux-gnu-" ;;
+      3\ *)
+      		x_TOOLCHAIN_SETUP_NAME="ARM gcc-arm-10.2-2020.11" ; x_TOOLCHAIN_SETUP="PATH=/opt/arm/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:\$PATH ; export CROSS_COMPILE=aarch64-none-elf-" ;;
+      0\ *)
+		x_TOOLCHAIN_SETUP_NAME="(none)" ; x_TOOLCHAIN_SETUP= ;;
+      *) whiptail --msgbox "Programmer error: unrecognized option" 20 60 1 ;;
+    esac || whiptail --msgbox "There was an error running option $SELECT" 20 60 1
+  fi
+
+  # Save our replies to some tmp file so other scripts can read it
+  echo "x_TOOLCHAIN_SETUP_NAME=\"$x_TOOLCHAIN_SETUP_NAME\"" > /tmp/toolchain_reply.txt
+  echo "x_TOOLCHAIN_SETUP=\"$x_TOOLCHAIN_SETUP\"" >> /tmp/toolchain_reply.txt
+  
+  exit
+fi
+
+# $1 = env variable to save
+# $2 = value
+# Remember, we we share this file with other scripts, so we only want to change
+# the lines used by this script
+save_setting() {
+
+
+  if [ ! -e $SETTINGS_FILE ] ; then
+    touch $SETTINGS_FILE # create file if does not exit
+  fi
+
+  # Do not change the file if we did not make any changes
+  grep -q "^$1=$2$" $SETTINGS_FILE
+  if [ "$?" == "0" ] ; then
+    return
+  fi
+
+  sed '/^'"$1"'=/d' -i $SETTINGS_FILE
+  echo  "$1=$2" >> $SETTINGS_FILE
+
+  # Delete empty or blank lines
+  sed '/^$/d' -i $SETTINGS_FILE
+
+  # Sort the file to keep the same order
+  sort -o $SETTINGS_FILE $SETTINGS_FILE
+}
+
+
+# Save Settings to file
+# Since each sub-script will want to save their settings, we will keep a common interface in this file.
+if [ "$1" == "save_setting" ] ; then
+
+  if [ "$SETTINGS_FILE" == "" ] ; then
+    echo -e "\nERROR: SETTINGS_FILE not set\n"
+    exit
+  fi
+
+  # Call the function in this file
+  save_setting "$2" "$3"
+
+  exit
+fi
+
+# Setting are kept in a board.ini file.
+# If you want to use a different board.in file, you can define it before you run this script
+#    $ export SETTINGS_FILE=my_board.ini
+#    $ ./build.sh
+
+if [ "$SETTINGS_FILE" == "" ] ; then
+  # If not set, use default file name
+  SETTINGS_FILE=board.ini
+  export SETTINGS_FILE=$SETTINGS_FILE
+
+  # Read in our settings
+  if [ -e "$SETTINGS_FILE" ] ; then
+    source $SETTINGS_FILE
+  fi
+fi
+
+if [ "$MACHINE" == "" ] && [ "$1" != "s" ] ; then
+  echo -e "\nERROR: No board selected. Please run \"./build.sh s\"\n"
+  exit
+fi
+
+
+#----------------------------------------------
+# Help Menu
+#----------------------------------------------
+if [ "$1" == "" ] ; then
+
+  echo "\
+
+Board: $MACHINE
+
+Please select what you want to build:
+
+  ./build.sh t                       # Build Trusted Firmware-A
+  ./build.sh u                       # Build u-boot
+  ./build.sh f                       # Build Renesas Flash Writer
+  ./build.sh k                       # Build Linux Kernel
+
+  ./build.sh s                       # Setup - Choose board and build options
+"
+  exit
+fi
+
+if [ "$1" == "t" ] ; then
+  ./build_tfa.sh $2 $3 $4
+  exit
+fi
+if [ "$1" == "u" ] ; then
+  ./build_uboot.sh $2 $3 $4
+  exit
+fi
+if [ "$1" == "f" ] ; then
+  ./build_flashwriter.sh $2 $3 $4
+  exit
+fi
+if [ "$1" == "k" ] ; then
+  ./build_kernel.sh $2 $3 $4
+  exit
+fi
+
+
+if [ "$1" == "s" ] ; then
+
+  SELECT=$(whiptail --title "Board Selection" --menu "You may use ESC+ESC to cancel." 0 0 0 \
+	"1  hihope-rzg2m" "HiHope RZ/G2M" \
+	"2  hihope-rzg2n" "HiHope RZ/G2N" \
+	"3  hihope-rzg2h" "HiHope RZ/G2H" \
+	"4  ek874" "Silicon Linux RZ/G2E" \
+	"5  smarc-rzg2l" "Renesas SMARC RZ/G2L" \
+	3>&1 1>&2 2>&3)
+  RET=$?
+  if [ $RET -eq 0 ] ; then
+    case "$SELECT" in
+      1\ *) FW_BOARD=HIHOPE ; MACHINE=hihope-rzg2m ;;
+      2\ *) FW_BOARD=HIHOPE ; MACHINE=hihope-rzg2n ;;
+      3\ *) FW_BOARD=HIHOPE ; MACHINE=hihope-rzg2h ;;
+      4\ *) FW_BOARD=EK874 ; MACHINE=ek874 ;;
+      5\ *) FW_BOARD=RZG2L_SMARC ; MACHINE=smarc-rzg2l ;;
+      *) whiptail --msgbox "Programmer error: unrecognized option" 20 60 1 ;;
+    esac || whiptail --msgbox "There was an error running option $SELECT" 20 60 1
+  else
+    # canceled
+    exit
+  fi
+
+  # Clear out the current settings file
+  echo "" > $SETTINGS_FILE
+
+  # Save our defaults
+  save_setting TFA_DIR $TFA_DIR_DEFAULT
+  save_setting UBOOT_DIR $UBOOT_DIR_DEFAULT
+  save_setting FW_DIR $FW_DIR_DEFAULT
+  save_setting KERNEL_DIR $KERNEL_DIR_DEFAULT
+
+  # The board
+  save_setting MACHINE $MACHINE
+  save_setting OUT_DIR output_${MACHINE}
+
+  # Set Default for Flash Writer script
+  save_setting FW_BOARD $FW_BOARD
+
+  # Set Default for Flash Writer script
+  if [ "$MACHINE" == "smarc-rzg2l" ] ; then
+    save_setting TFA_FIP 1
+  else
+    save_setting TFA_FIP 0
+  fi
+
+fi
---
+++ build_scripts_new/build_common.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#########################################################
+# This file contains functions common to all the scripts
+# Each script sour 'source' this file at the beginning.
+#########################################################
+
+
+
+# Toolchain Selection GUI
+# Since each sub-script will want to ask the user what toolchain to use, we will keep a common interface in this file.
+# $1 = env variable to save TOOLCHAIN_SETUP_NAME
+# $2 = env variable to save TOOLCHAIN_SETUP
+select_toolchain() {
+
+    SELECT=$(whiptail --title "Toolchain setup" --menu "You may use ESC+ESC to cancel.\nEnter the command line you want to run before build.\n" 0 0 0 \
+	"1  SDK Toolchain (Rocko/Poky 2.4.3)" "  /opt/poky/2.4.3/environment-setup-aarch64-poky-linux" \
+	"2  Linaro gcc-linaro-7.5.0-2019.12" "  /opt/linaro/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu" \
+	"3  ARM gcc-arm-10.2-2020.11" "  /opt/arm/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf" \
+	"0  (none)" "  No setup" \
+	3>&1 1>&2 2>&3)
+  RET=$?
+  if [ $RET -eq 0 ] ; then
+    case "$SELECT" in
+      1\ *)
+      		x_TOOLCHAIN_SETUP_NAME="SDK Toolchain (Poky 2.4.3)"
+      		x_TOOLCHAIN_SETUP="source /opt/poky/2.4.3/environment-setup-aarch64-poky-linux" ;;
+      2\ *)
+      		x_TOOLCHAIN_SETUP_NAME="Linaro gcc-linaro-7.5.0-2019.12"
+      		x_TOOLCHAIN_SETUP="PATH=/opt/linaro/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin:\$PATH ; export CROSS_COMPILE=aarch64-linux-gnu-" ;;
+      3\ *)
+      		x_TOOLCHAIN_SETUP_NAME="ARM gcc-arm-10.2-2020.11"
+      		x_TOOLCHAIN_SETUP="PATH=/opt/arm/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:\$PATH ; export CROSS_COMPILE=aarch64-none-elf-" ;;
+      0\ *)
+		x_TOOLCHAIN_SETUP_NAME="(none)"
+		x_TOOLCHAIN_SETUP= ;;
+      *) whiptail --msgbox "Programmer error: unrecognized option" 20 60 1 ;;
+    esac || whiptail --msgbox "There was an error running option $SELECT" 20 60 1
+  fi
+
+  DO_SET="export $1=\"$x_TOOLCHAIN_SETUP_NAME\""
+  eval "$DO_SET"
+
+  DO_SET="export $2=\"$x_TOOLCHAIN_SETUP\""
+  eval "$DO_SET"
+}
+
+
+# $1 = env variable to save
+# $2 = value
+# Remember, we we share this file with other scripts, so we only want to change
+# the lines used by this script
+save_setting() {
+
+
+  if [ ! -e $SETTINGS_FILE ] ; then
+    touch $SETTINGS_FILE # create file if does not exit
+  fi
+
+  # Do not change the file if we did not make any changes
+  grep -q "^$1=$2$" $SETTINGS_FILE
+  if [ "$?" == "0" ] ; then
+    return
+  fi
+
+  sed '/^'"$1"'=/d' -i $SETTINGS_FILE
+  echo  "$1=$2" >> $SETTINGS_FILE
+
+  # Delete empty or blank lines
+  sed '/^$/d' -i $SETTINGS_FILE
+
+  # Sort the file to keep the same order
+  sort -o $SETTINGS_FILE $SETTINGS_FILE
+}
+
+
