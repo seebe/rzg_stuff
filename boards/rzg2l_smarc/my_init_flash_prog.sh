c702f25fc0af071aea0bbb333748e38ee3717ce8
micbis <michele.bisogno.ct@renesas.com>
2022-04-04 11:29:29 +0200

    eMMC init script: added u-boot env variables set thru fw_setenv

--- boards/rzg2l_smarc/my_init_flash_prog.sh
+++ boards/rzg2l_smarc/my_init_flash_prog.sh
@@ -4,6 +4,7 @@ USB_DRIVE=/dev/sda1
 FILE_NAME=sd_card.img.gz
 BL2=/home/root/bl2_bp.bin
 FIP=/home/root/fip.bin
+ENV_FINAL_VAR=/home/root/final_env_variables.txt
 # Populate /proc /sys /dev
 mount -t proc proc /proc
 mount -t sysfs sysfs /sys
@@ -22,6 +23,9 @@ sync
 echo "Enlarging ext partition to fit the eMMC size..."
 echo ", +" | sfdisk -N 2 ${DISK}
 resize2fs "${DISK}p2"
+# You may not need to reprogram BL2 and FIP,
+# you may just want to modify environment variables however
+# this script does both things for the sake of completeness.
 echo "Reprogramming BL2 and FIP"
 echo "onto eMMC boot partition 0..."
 echo 0 > /sys/block/mmcblk0boot0/force_ro  # disable the soft write-protect base
@@ -32,6 +36,10 @@ dd if=/dev/zero of=boot.img bs=1024 count=1024
 dd if=${BL2} of=boot.img conv=notrunc
 dd if=${FIP} of=boot.img conv=notrunc bs=512 seek=233
 flashcp -v boot.img /dev/mtd0
+echo "Resetting environment variables"
+fw_setenv --script ${ENV_FINAL_VAR}
+echo "Printing environment variables.."
+fw_printenv
 echo "Done"
 echo "Wait 5s before reboot"
 sleep 5
