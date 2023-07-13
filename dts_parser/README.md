6760ee5fcad835ae011f7579d29cf2399c146876
Chris Brandt <chris.brandt@renesas.com>
2023-07-13 18:01:03 -0400

    add dts_parser

---
+++ dts_parser/README.md
@@ -0,0 +1,108 @@
+You pass the file name of a Device Tree .dts file, and then it will list all the #include files so you can know all the files that are include, and in what order.
+
+Also, for each file, it will print out what new nodes were created, as well as nodes that are being modifed/added.
+
+Example:
+
+<pre>
+$ cd rz_linux-cip/arch/arm64/boot/dts/renesas
+
+$ wget https://raw.githubusercontent.com/seebe/rzg_stuff/master/dts_parser/dts_parser.py
+
+$ python3 dts_parser.py r9a07g054l2-smarc.dts
+
+-----------------------------------
+Include order
+-----------------------------------
+ 
+r9a07g054l2-smarc.dts
+└─r9a07g054l2.dtsi
+  └─r9a07g054.dtsi
+└─rzg2l-smarc-som.dtsi
+└─rzg2l-smarc-pinfunction.dtsi
+└─rz-smarc-common.dtsi
+└─rzg2l-smarc.dtsi
+ 
+ 
+-------------------------------------------------------------
+Order of how files and nodes are parsed.
+-------------------------------------------------------------
+Remember, the #include is at the top of the files and the nodes come after. So that means the last file
+that gets included will have the nodes that get processed first. This is important to know when you have
+multiple files that modify the same node.
+ 
+r9a07g054.dtsi
+  nodes new: 
+	audio_clk1		audio_clk2		can_clk			extal_clk		
+	firmware		cluster0_opp		cpus			psci			
+	gpu_opp_table		soc			thermal-zones		timer			 
+  nodes override: 
+	 
+ 
+r9a07g054l2.dtsi
+  nodes new: 
+	 
+  nodes override: 
+	 
+ 
+rzg2l-smarc-som.dtsi
+  nodes new: 
+	aliases			chosen			memory@48000000		reserved-memory		
+	mmngr			mmngrbuf		vspm_if			reg_1p8v		
+	reg_3p3v		reg_1p1v		vccq_sdhi0		phy0			
+	phy1			adc_pins		eth0_pins		eth1_pins		
+	gpio-sd0-pwr-en-hog	qspi0_pins		sd0-dev-sel-hog		sdhi0_emmc_pins		
+	sdhi0_pins		sdhi0_pins_uhs		flash@0			pmic			
+	rtc			 
+  nodes override: 
+	&adc			&eth0			&eth1			&extal_clk		
+	&gpu			&ostm1			&ostm2			&pinctrl			
+	&sbc			&sdhi0			&sdhi0			&wdt0			
+	&wdt1			&wdt2			&poega			&poegb			
+	&poegc			&poegd			&gpt4			&ehci0			
+	&ohci0			&ehci1			&ohci1			&i2c3			 
+ 
+rzg2l-smarc-pinfunction.dtsi
+  nodes new: 
+	can0_pins		can0-stb-hog		can1_pins		can1-stb-hog		
+	i2c0_pins		i2c1_pins		i2c3_pins		mtu3_pins		
+	scif0_pins		scif2_pins		sd1-pwr-en-hog		sdhi1_pins		
+	sdhi1_pins_uhs		sound_clk_pins		spi1_pins		ssi0_pins		
+	ssi1_pins		usb0_pins		usb1_pins		gpt4_pins		 
+  nodes override: 
+	&pinctrl			 
+ 
+rz-smarc-common.dtsi
+  nodes new: 
+	aliases			chosen			snd_rzg2l		usb0_vbus_otg		
+	vccq_sdhi1		hdmi-out		clk_ext_camera		ov5645_vdddo_1v8	
+	ov5645_vdda_2v8		ov5645_vddd_1v5		channel0		channel1		
+	ov5645			adv7535			ports			ports			 
+  nodes override: 
+	&audio_clk1		&audio_clk2		&canfd			&ehci0			
+	&ehci1			&hsusb			&i2c0			&i2c1			
+	&ohci0			&ohci1			&phyrst			&scif0			
+	&sdhi1			&spi1			&usb2_phy0		&usb2_phy1		
+	&du			&dsi0			&cru			&csi2			
+	&mtu3			 
+ 
+rzg2l-smarc.dtsi
+  nodes new: 
+	aliases			sound_card		x1_clk			versa3			
+	wm8978			ports			mtu3_ch34		 
+  nodes override: 
+	&cpu_dai			&codec_dai		&i2c3			&scif2			
+	&ssi0			&ssi1			&vccq_sdhi1		&ov5645			
+	&adv7535			&mtu3			&poe3			 
+ 
+r9a07g054l2-smarc.dts
+  nodes new: 
+	reserved-memory		 
+  nodes override: 
+	 
+
+
+</pre>
+
+
+
---
+++ dts_parser/dts_parser.py
@@ -0,0 +1,175 @@
+import sys
+import os
+import re
+
+level = 0
+file_list_names = []
+file_list_indent = []
+file_list_nodes_modified = []
+file_list_nodes_new = []
+
+def find_includes(file_path, directory):
+    include_files = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            match = re.search(r'#include\s+"([^"]+)"', line)
+            if match:
+                include_file = match.group(1)
+                if os.path.exists(os.path.join(directory, include_file)):
+                    include_files.append(include_file)
+    return include_files
+
+def find_nodes_modified(file_path):
+    nodes = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            match = re.search(r'&(\w+)\s*{', line)
+            if match:
+                nodes.append(match.group(1))
+    return nodes
+
+def find_nodes_new(file_path):
+    nodes = []
+    braces_level = 0
+    with open(file_path, 'r') as f:
+        for line in f:
+            if line.find("{") != -1:
+                braces_level = braces_level + 1
+            if line.find("}") != -1:
+                braces_level = braces_level - 1
+            if line.find("&") != -1: # skip overide nodes
+                continue
+            if line.find("{") != -1:
+                if braces_level == 2:
+                    line = line.replace("{","") # remove braces
+                    # does node have a label?
+                    if line.find(":") != -1:
+                        #node has label, use that
+                        line = re.sub(":.*",'', line)
+                    line = line.lstrip()
+                    line = line.rstrip()
+                    nodes.append(line)
+    return nodes
+
+
+def process_file(file_path, indent=0):
+    directory = os.path.dirname(file_path)
+    include_files = find_includes(file_path, directory)
+    nodes_modified = find_nodes_modified(file_path)
+
+    #print(' ' * indent + '[' + os.path.basename(file_path) + ']')
+    if indent == 2:
+        print('└─', end='')
+    if indent == 4:
+        print('  └─', end='')
+    if indent == 6:
+        print('    └─', end='')
+    if indent == 8:
+        print('      └─', end='')
+    #print('[' + os.path.basename(file_path) + ']')
+    print(os.path.basename(file_path))
+    #print(' ' * indent + '[' + os.path.basename(file_path) + ']')
+    for include_file in include_files:
+        process_file(os.path.join(directory, include_file), indent + 2)
+
+    # Build the list in reverse order
+    file_list_names.append(os.path.basename(file_path))
+
+    # Find override nodes iwth &
+    node_list = find_nodes_modified(file_path)
+    node_list_str = ""
+    count = 0
+    node_list_str = node_list_str + "\n\t"
+    for node in node_list:
+        if count == 4:
+            node_list_str = node_list_str + "\n\t"
+            count = 0
+        count = count + 1
+        node_list_str = node_list_str + "&" + node
+        node_list_str = node_list_str + "\t"
+        if (len(node) < 8):
+            node_list_str = node_list_str + "\t"
+        if (len(node) < 16):
+            node_list_str = node_list_str + "\t"
+    file_list_nodes_modified.append(node_list_str)
+
+    # Find new nodes (no &)
+    node_list = find_nodes_new(file_path)
+    node_list_str = ""
+    count = 0
+    node_list_str = node_list_str + "\n\t"
+    for node in node_list:
+        if count == 4:
+            node_list_str = node_list_str + "\n\t"
+            count = 0
+        count = count + 1
+        node_list_str = node_list_str + node
+        node_list_str = node_list_str + "\t"
+        if (len(node) < 8):
+            node_list_str = node_list_str + "\t"
+        if (len(node) < 16):
+            node_list_str = node_list_str + "\t"
+    file_list_nodes_new.append(node_list_str)
+
+
+#    if indent == 2:
+#        print('  └─', end='')
+#    if indent == 4:
+#        print('    └─', end='')
+#    if indent == 6:
+#        print('      └─', end='')
+#    if indent == 8:
+#        print('        └─', end='')
+#    for node in nodes:
+#        #print(' ' * (indent + 2) + node)
+#        print('&' + node + ", ", end='')
+#    print("")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <path_to_dts_file>")
+        sys.exit(1)
+
+    dts_file = sys.argv[1]
+    if not os.path.isfile(dts_file) or not dts_file.endswith('.dts'):
+        print("Please provide a valid .dts file path")
+        sys.exit(1)
+
+    print("-----------------------------------")
+    print("Include order")
+    print("-----------------------------------")
+    print(" ")
+
+    process_file(dts_file)
+
+    print(" ")
+    print(" ")
+    print("-------------------------------------------------------------")
+    print("Order of how files and nodes are parsed.")
+    print("-------------------------------------------------------------")
+    print("Remember, the #include is at the top of the files and the nodes come after. So that means the last file")
+    print("that gets included will have the nodes that get processed first. This is important to know when you have")
+    print("multiple files that modify the same node.")
+    print(" ")
+
+
+    count = len(file_list_names)
+    for i in range(count):
+        #print(file_list_names[i], end='')
+        print(file_list_names[i] + "")
+        #print("  nodes override: ", end='')
+        #print(file_list_nodes_modified[i], end='')
+        #print(" ")
+        #print("  nodes new: ", end='')
+        #print(file_list_nodes_new[i], end='')
+        print("  nodes new: ", end='')
+        print(file_list_nodes_new[i], end='')
+        print(" ")
+        print("  nodes override: ", end='')
+        print(file_list_nodes_modified[i], end='')
+        print(" ")
+        print(" ")
+#    print(count)
+    #print(file_list_names)
+    #print(file_list_nodes_modified)
+
